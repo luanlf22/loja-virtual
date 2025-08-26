ea7c8e3c90d5e6777a511ff2867ba156e513f47e
luanlf22 <luanlf2222@gmail.com>
2025-08-26 20:11:13 -0300

    alteracao

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'exporess';
+import { use } from 'react';
+const app = express();
+app.use(express.json());
+const User =[]
+app.get('/usuarios',(req,res)=>{
+
+    userInfo.push(req.body)
+    app.get('/usuarios',(req,res)=>{
+        res.json(userInfo)
+    res.send('tudo certo')
+    app.listen(3000)
+app.post('/usuarios')})});
